c078d4f9c90b2d65d49cf009f8f0e20f7af9746b
Dishant Singh <65276766+dishantsingh1998@users.noreply.github.com>
2021-05-19 17:15:13 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,604 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "dishantsingh1998",
+  logo_name: "dishant.s()",
+  nickname: "Dishant",
+  full_name: "Dishant Singh",
+  subTitle:
+    "Full Stack Developer, Competitive Coder, Open Source Enthusiast 🔥. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1dgNyFbyqsAnQ8TD3JZL6THesT-mKVy5R/view?usp=sharing",
+  mail: "dishantsingh1998@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/dishantsingh1998",
+  linkedin: "https://www.linkedin.com/in/dishant-singh-6b05a7138/",
+  gmail: "dishantsingh1998@gmail.com",
+  gitlab: "https://gitlab.com/dishantsingh1998",
+  twitter: "https://twitter.com/Dishantsingh5",
+  instagram: "https://www.instagram.com/dishantsingh1189/",
+  codechef: "https://www.codechef.com/users/dishantsingh27",
+  hackerrank: "https://www.hackerrank.com/dishantsingh1998",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Creating application backend in Node, Express & MongoDB",
+        "⚡ Integration of third party services such as  AWS, Netlify and Heroku",
+        "⚡ Integration of Payment Gateaway such Stripe,Paypal and Paytm",
+        "⚡ Styling Website with Ant-Design,Material-Ui and Bootstrap",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+
+        {
+          skillName: "GraphQL",
+          fontAwesomeClassname: "simple-icons:graphql",
+          style: {
+            color: "#DE33A6",
+          },
+        },
+
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Thakur College of Engineering and Technology",
+      subtitle:
+        "Bachelors of Engineering in Information Technology , CGPI - 7.4",
+      logo_path: "tcet.jpg",
+      alt_name: "TCET",
+      duration: "2016 - 2020",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for Frontend ,Backend , Web , Mobile App Development, AI , IOT etc.",
+        "⚡ I have implemented several projects based on what I've leart under my Undergraduate course. ",
+      ],
+      website_link: "https://www.tcetmumbai.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Python Data Structures",
+      subtitle: "University of Michigan",
+      logo_path: "mongo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1YXemOz8olD8TRgjcuykOyx4l4wQYqY5L/view?usp=sharing",
+      alt_name: "University of Michigan",
+      color_code: "#F0FFFF",
+    },
+    {
+      title: "Satellite Photogrammetry and its Application",
+      subtitle: "ISRO",
+      logo_path: "isro.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1kfcK9OIm1MoNtGZ_8p5KUwbAd5-V2ASh/view?usp=sharing",
+      alt_name: "ISRO",
+      color_code: "#F0FFFF",
+    },
+    {
+      title: "Tableau 2020 A-Z:Hand-On Tableau Training For Data Science",
+      subtitle: "Udemy",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1Pp8nGAEY79Uu3LHx2BZAsf-nD17ZFkkI/view?usp=sharing",
+      alt_name: "Udemy",
+      color_code: "#F0FFFF",
+    },
+    {
+      title: "Data Analytics Consulting Virtual Internship",
+      subtitle: "KPMG",
+      logo_path: "kpmg.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1z2TY3EuO5PmRDMtQ9nGdxqc9_bnd152r/view?usp=sharing",
+      alt_name: "kpmg",
+      color_code: "#F0FFFF",
+    },
+    {
+      title: "Google Web Designer Basics",
+      subtitle: "Google",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1E3RG0weGnc366hGVIROeEGPcN4Jdz7dt/view?usp=sharing",
+      alt_name: "Google",
+      color_code: "#F0FFFF",
+    },
+    {
+      title: "Google Analytics Individual Qualification",
+      subtitle: "Google",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1MNs9NjEltGnJvZYZb4Va4KfbN0xCMKzs/view?usp=sharing",
+      alt_name: "Google",
+      color_code: "#F0FFFF",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    " I've mostly done projects on my own . I love sharing my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "GitHub Student Developer",
+          company: "GitHub",
+          company_url: "https://github.com/",
+          logo_path: "github.png",
+          duration: "Nov 2019 - Present",
+          location: "Work from Home",
+          description:
+            "Contribute to Open Source Community and Open Source Project.",
+          color: "#040f26",
+        },
+        {
+          title: "Google Local Guide",
+          company: "Google Map",
+          company_url: "https://maps.google.com/localguides/",
+          logo_path: "localguide.png",
+          duration: "Sep 2018 - Present",
+          location: "Work From Home",
+          description:
+            "Day-to-day responsibilities of helping local businesses to spread their business to the world. Helping users by writing reviews about different locations and spaces such as shops, malls, etc.",
+          color: "#D83B01",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, FrontEnd Projects, and React Project. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "dishant.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://twitter.com/Dishantsingh5",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Classify Flower Species",
+      url: "https://github.com/dishantsingh1998/Classify-Flower-Species",
+      description:
+        "This is an example of a notebook to demonstrate concepts of Data Science. In this example we will do some exploratory data analysis on the famous Iris dataset.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Amazon Fine Food Review",
+      url: "https://github.com/dishantsingh1998/Amazon-Fine-Food-Review",
+      description:
+        "This is an example of a notebook to demonstrate concepts of Data Science. In this example we will do some exploratory data analysis on the famous Amazon fine food.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "MERN CRUD APPLICATION",
+      url: "https://github.com/dishantsingh1998/MERN_CRUD",
+      description: "A Simple Application to perform CRUD function",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "React Weather Application",
+      url: "https://github.com/dishantsingh1998/REACT-WEATHER",
+      description: "Weather App using AccuWeather API        ",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "Currency Convertor",
+      url: "https://github.com/dishantsingh1998/CURRENCY-CONVERTOR",
+      description:
+        "Created a Currency Convertor Web Application with Exchangerates API",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "5",
+      name: "personal-portfolio",
+      url: "https://github.com/dishantsingh1998/personal-portfolio",
+      description:
+        "A simple command line interface based quiz app to know more about me :).",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "6",
+      name: "Dream Property",
+      url: "https://github.com/dishantsingh1998/dreamproperty",
+      description:
+        "Created a Real Estate Website by which Customer can look the best property in the city",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "7",
+      name: "GAMESTORE",
+      url: "https://github.com/dishantsingh1998/GAMESTORE",
+      description:
+        "Created a GameStore Website by which Customer can look trending games and website is Integrated with Stripe Payment Gateaway",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "8",
+      name: "To-Do App with passport and Jwt-Token",
+      url:
+        "https://github.com/dishantsingh1998/To-do-App-with-Passport-and-Jwt",
+      description: "To-Do App with passport and Jwt-Token",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      id: "9",
+      name: "SpaceX API Fetch",
+      url: "https://github.com/dishantsingh1998/SpaceX-Apollo-GraphQl",
+      description:
+        "Fetch and Displayed SpaceX Api with Graphql and Apollo-Client",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "GraphQL",
+          iconifyClass: "logos-graphql",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
